c5b3a55f362420b7437f25c227b5957d3021d74c
Megumikawa <megumi19901016@gmail.com>
2021-09-04 22:14:29 +0200

    answered

--- 7kyu/drone_fly_by.js
+++ 7kyu/drone_fly_by.js
@@ -16,5 +16,22 @@
 
 
 function flyBy(lamps, drone){
+  let lampArr = lamps.split('')
+  for(let i = 0; i < drone.length; i++) {
+    if (lampArr[i] === 'x') {
+      lampArr.splice(i, 1, 'o')
+    }
+  }
+  return lampArr.join('')
+}
 
+// Another answer
+const flyBy = (lamps, drone) => [...lamps].fill(`o`, 0, drone.length).join(``)
+
+
+
+// Another answer
+function flyBy(lamps, drone){
+  if(drone.length > lamps.length) return 'o'.repeat(lamps.length)
+  return lamps.replace('x'.repeat(drone.length), 'o'.repeat(drone.length))
 }
