2e21767d8e2726a41d54c67d72b3489ff318db3f
Megumikawa <megumi19901016@gmail.com>
2021-09-05 21:25:53 +0200

    answered

--- 8kyu/sum_withoug_highest_and_lowest_number.js
+++ 8kyu/sum_withoug_highest_and_lowest_number.js
@@ -6,3 +6,35 @@
 // Example:
 // { 6, 2, 1, 8, 10 } => 16
 // { 1, 1, 11, 2, 3 } => 6
+
+
+
+const sumArray = array => {
+  if(!array){
+    return 0
+  }
+  let sum = 0;
+  array.sort((a,b) => a - b).forEach((a, i) => {
+    if(i !== array.length - 1 && i !== 0) {
+      sum += a
+    }
+  })
+  return sum
+}
+
+// Another answer
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
