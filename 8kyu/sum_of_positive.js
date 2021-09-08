c4c3255f90ea70360efeef3e9f985e802d05c3c9
Megumikawa <megumi19901016@gmail.com>
2021-09-08 16:35:09 +0200

    answered

--- 8kyu/sum_of_positive.js
+++ 8kyu/sum_of_positive.js
@@ -2,3 +2,22 @@
 
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
+
+
+const positiveSum = (arr) => {
+  let count = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] >= 0) {
+      count += arr[i]
+    }
+  }
+  return count;
+}
+
+// Another way
+function positiveSum(arr) {
+  return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
+
+// Another way
+const positiveSum = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0);
