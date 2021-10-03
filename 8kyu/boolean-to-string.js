b6d0814d90578efa8e082f6c853fd6d8876c91f5
Megumikawa <megumi19901016@gmail.com>
2021-10-03 21:48:29 +0200

    answer

--- 8kyu/boolean-to-string.js
+++ 8kyu/boolean-to-string.js
@@ -1,5 +1,19 @@
 // Implement a function which convert the given boolean value into its string representation.
 
 function booleanToString(b){
-  //your code here
+  if(b === true) {
+    return "true"
+  } else {
+    return "false"
+  }
+}
+
+// Other answer
+function booleanToString(b){
+  return b.toString();
+}
+
+// Another answer
+function booleanToString(b){
+  return b ? 'true' : 'false';
 }
