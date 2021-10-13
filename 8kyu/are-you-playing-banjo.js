307f8a7396c52b0f6e4f7a2cff1943a8ad3e218e
Megumikawa <megumi19901016@gmail.com>
2021-10-13 08:29:35 +0200

    todays Kata

--- 8kyu/are-you-playing-banjo.js
+++ 8kyu/are-you-playing-banjo.js
@@ -5,7 +5,22 @@
 // name + " plays banjo" 
 // name + " does not play banjo"
 
+
+function areYouPlayingBnajo(name) {
+  if(name.toLowerCase().charAt(0) === 'r'){
+    name = name + " play banjo"
+  } else {
+    name = name + " does not play banjo"
+  }
+  return name
+}
+
+// Another 
 function areYouPlayingBanjo(name) {
-  // Implement me
-  return name;
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+
+// Another
+function areYouPlayingBanjo(name) {
+  return name[0].toLowerCase() == "r" ? name + " plays banjo" : name + " does not play banjo";
 }
