a62410df0d68dfcefac8c05d7e43feb3f6e7f3a6
Megumikawa <megumi19901016@gmail.com>
2021-10-19 09:04:47 +0200

    practice

--- 8kyu/convert-boolean-values-to-strings.js
+++ 8kyu/convert-boolean-values-to-strings.js
@@ -1,6 +1,15 @@
 // Complete the method that takes a boolean value and 
 // return a "Yes" string for true, or a "No" string for false.
 
+
+// my answer
 function boolToWord( bool ){
-  //...
+  if(bool === true){
+    return 'Yes'
+  } else {
+    return 'No'
+  }
 }
+
+// My answer
+const boolToWord = (bool) => bool === true ? 'Yes' : 'No';
